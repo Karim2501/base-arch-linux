5d1d3d4411dbae2aa09512e91fc76cbaf2f44c4c
karim <karimmitra0@gmail.com>
2021-05-28 16:19:07 +0700

    edit qtile file

--- dotfiles-myqtile/install-on-arch.sh
+++ dotfiles-myqtile/install-on-arch.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-sudo timedatectl set-ntp true
-sudo hwclock --systohc
 
 sudo reflector -c Indonesia -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -S --noconfirm xorg firefox obs-studio vlc kdenlive alacritty code inkscape gimp libreoffice shotcut qtile ranger gparted htop zsh
 
-sudo pacman -S --noconfirm xorg firefox obs-studio vlc kdenlive alacritty code inkscape gimp libreoffice shotcut qtile
-
-/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
 sleep 5
 reboot
