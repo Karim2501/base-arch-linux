dca3ced44b151b3f6b641a5bf02a65761a63b095
karim <karimmitra0@gmail.com>
2021-05-28 16:35:22 +0700

    edit qtile file

--- dotfiles-myqtile/install-on-arch.sh
+++ dotfiles-myqtile/install-on-arch.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+#Update mirrorlist 
 sudo reflector -c Indonesia -a 12 --sort rate --save /etc/pacman.d/mirrorlist
-sudo pacman -S --noconfirm xorg firefox obs-studio vlc kdenlive alacritty code inkscape gimp libreoffice shotcut qtile ranger gparted htop zsh
 
+#Refresh pacman
+sudo pacman -noconfirm -Syyu
+
+#Install package
+sudo pacman -S --noconfirm xorg xorg-xinit xorg-xinput qtile zsh wget git feh ranger gparted htop firefox alacritty obs-studio vlc kdenlive code inkscape gimp libreoffice shotcut
+
+#Make directory
+mkdir -p ~/.local/share/fonts
+mkdir -p ~/.srcs
+mkdir -p ~/.config
+
+#Install paru
+
+#Install yay
+
+#Install oh-myzsh
+
+#Install powerlevel10k
 echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
 sleep 5
 reboot
