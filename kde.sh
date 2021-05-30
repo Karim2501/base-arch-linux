e43afc6cd6093c0c9725d2e5a5a6140f8bf91a05
karim <karimmitra0@gmail.com>
2021-05-30 22:28:03 +0700

    edit 5

--- kde.sh
+++ kde.sh
@@ -3,7 +3,8 @@
 sudo reflector -c Indonesia -a 12 --sort rate --save /etc/pacman.d/mirrorlist
 
 sudo pacman -Syyu
-sudo pacman -S sddm plasma plasma-wayland-session kde-applications
+sudo pacman -S --noconfirm sddm
+sudo pacman -S --noconfirm plasma plasma-wayland-session kde-applications
 
 sudo systemctl enable sddm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
