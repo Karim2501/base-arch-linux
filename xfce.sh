78fd311a41794507060590774f1d44cdfdb7e0e0
karim <karimmitra0@gmail.com>
2021-05-30 17:27:23 +0700

    edit 4

--- xfce.sh
+++ xfce.sh
@@ -3,7 +3,8 @@
 sudo reflector -c Indonesia -a 12 --sort rate --save /etc/pacman.d/mirrorlist
 
 sudo pacman -Syyu
-sudo pacman -S --noconfirm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 xfce4-goodies arc-gtk-theme arc-icon-theme obs-studio
+sudo pacman -S --noconfirm lightdm
+sudo pacman -S --noconfirm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 xfce4-goodies arc-gtk-theme arc-icon-theme obs-studio
 
 sudo systemctl enable lightdm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
